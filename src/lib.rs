f41264d52144ee6e5839cb6ce250491330a0b691
FuNn1esT <admin@n1x.cn>
2019-06-05 00:21:23 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 mod n198_house_robber;
 mod n344_reverse_string;
+mod n461_hamming_distance;
---
+++ src/n461_hamming_distance/mod.rs
@@ -0,0 +1,27 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        format!("{:b}", x ^ y)
+            .chars()
+            .fold(0, |acc, x| if x == '1' { acc + 1 } else { acc })
+    }
+    pub fn hamming_distance1(x: i32, y: i32) -> i32 {
+        (x ^ y).count_ones() as i32
+    }
+    pub fn hamming_distance2(x: i32, y: i32) -> i32 {
+        format!("{:b}", x ^ y).matches('1').count() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_hamming_distance() {
+        assert_eq!(Solution::hamming_distance(1, 4), 2);
+        assert_eq!(Solution::hamming_distance1(1, 4), 2);
+        assert_eq!(Solution::hamming_distance2(1, 4), 2);
+    }
+}
