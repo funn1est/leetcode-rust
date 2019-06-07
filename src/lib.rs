c027877459740a5f2d46de6d312aefc9741ebd1c
FuNn1esT <admin@n1x.cn>
2019-06-07 23:50:36 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod n62_unique_paths;
 mod n118_pascals_triangle;
 mod n198_house_robber;
 mod n268_missing_number;
---
+++ src/n62_unique_paths/mod.rs
@@ -0,0 +1,52 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        // dp
+        let m = m as usize;
+        let n = n as usize;
+        let mut dp = vec![vec![1; n]; m];
+        for i in 1..m {
+            for j in 1..n {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        dp[m - 1][n - 1]
+    }
+    pub fn unique_paths1(m: i32, n: i32) -> i32 {
+        // recursion
+        if m == 1 || n == 1 {
+            return 1;
+        }
+        Solution::unique_paths1(m - 1, n) + Solution::unique_paths1(m, n - 1)
+    }
+    pub fn unique_paths2(m: i32, n: i32) -> i32 {
+        // recursion + memorization
+        let m = m as usize;
+        let n = n as usize;
+        fn helper(m: usize, n: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
+            if m == 0 || n == 0 {
+                return 1;
+            } else if memo[m][n] == -1 {
+                memo[m][n] = helper(m - 1, n, memo) + helper(m, n - 1, memo);
+            }
+            memo[m][n]
+        }
+        helper(m - 1, n - 1, &mut vec![vec![-1; n]; m])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_unique_paths() {
+        assert_eq!(Solution::unique_paths(3, 2), 3);
+        assert_eq!(Solution::unique_paths(7, 3), 28);
+        assert_eq!(Solution::unique_paths1(3, 2), 3);
+        assert_eq!(Solution::unique_paths1(7, 3), 28);
+        assert_eq!(Solution::unique_paths2(3, 2), 3);
+        assert_eq!(Solution::unique_paths2(7, 3), 28);
+    }
+}
