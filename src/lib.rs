d60d7d1b54462e857462adbd808edf1be6968ae0
FuNn1esT <admin@n1x.cn>
2019-06-16 13:42:12 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod n49_group_anagrams;
 mod n53_maximum_subarray;
 mod n62_unique_paths;
 mod n118_pascals_triangle;
+mod n172_factorial_trailing_zeroes;
 mod n198_house_robber;
 mod n268_missing_number;
 mod n344_reverse_string;
