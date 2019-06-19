8c498e09b1f93f214696a798729c5975471e698a
FuNn1esT <admin@n1x.cn>
2019-06-19 23:57:16 +0800

    update

---
+++ src/n3_longest_substring_without_repeating_characters/mod.rs
@@ -0,0 +1,49 @@
+use std::cmp;
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let n = s.len();
+        if n == 0 {
+            return 0;
+        }
+
+        let mut max = 0;
+        let mut map: HashMap<char, i32> = HashMap::new();
+        let mut start = 0;
+
+        for (end, key) in s.chars().enumerate() {
+            if let Some(index) = map.get(&key) {
+                start = cmp::max(start, map.get(&key).unwrap() + 1);
+            }
+            map.insert(key, end as i32);
+            max = cmp::max(max, end as i32 - start + 1);
+        }
+
+        max
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_length_of_longest_substring() {
+        assert_eq!(
+            Solution::length_of_longest_substring("abcabcbb".to_string()),
+            3
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring("bbbbb".to_string()),
+            1
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring("pwwkew".to_string()),
+            3
+        );
+        assert_eq!(Solution::length_of_longest_substring("abba".to_string()), 2);
+    }
+}
