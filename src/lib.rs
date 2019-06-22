c1c2b8c8360986d6400e7d17c81d9a3d203eb58a
FuNn1esT <admin@n1x.cn>
2019-06-22 23:50:44 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod libs;
 
 mod n2_add_two_numbers;
 mod n3_longest_substring_without_repeating_characters;
+mod n9_palindrome_number;
 mod n49_group_anagrams;
 mod n53_maximum_subarray;
 mod n62_unique_paths;
---
+++ src/n9_palindrome_number/mod.rs
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/palindrome-number/
+// https://leetcode-cn.com/problems/palindrome-number/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        // https://stackoverflow.com/a/24542608
+        if x < 0 {
+            return false;
+        }
+        let string = x.to_string();
+        let half = string.len() / 2;
+        string
+            .bytes()
+            .take(half)
+            .eq(string.bytes().rev().take(half))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_is_palindrome() {
+        assert_eq!(Solution::is_palindrome(121), true);
+        assert_eq!(Solution::is_palindrome(-121), false);
+        assert_eq!(Solution::is_palindrome(10), false);
+    }
+}
