463ec446b5b0262a89f4fb475aee3d8732d0cb6f
FuNn1esT <admin@n1x.cn>
2019-06-26 23:52:00 +0800

    update

--- src/n13_roman_to_integer/mod.rs
+++ src/n13_roman_to_integer/mod.rs
@@ -7,19 +7,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
+        let map: HashMap<char, i32> = [
+            ('M', 1000),
+            ('D', 500),
+            ('C', 100),
+            ('L', 50),
+            ('X', 10),
+            ('V', 5),
+            ('I', 1),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
         let mut sum = 0;
         let mut last = 0;
+
         for char in s.chars() {
-            let val = match char {
-                'M' => 1000,
-                'D' => 500,
-                'C' => 100,
-                'L' => 50,
-                'X' => 10,
-                'V' => 5,
-                'I' => 1,
-                _ => 0,
-            };
+            let &val = map.get(&char).unwrap();
             if val > last {
                 sum += val - last - last;
             } else {
